917ead13f2de97ffbf608eae7de2a0cb119a298b
Arynn Boniface <arynn.boniface@gmail.com>
2021-07-13 17:00:11 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express(); 
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+// app: underlying express app to register this route handler with
+// get: watch for incoming requests with this method
+// '/': watch for requests trying to access '/'
+// req: object representing the incoming request
+// res: object representing the outgoing response
+// res.send({ hi: 'there'}): immediately send some JSON back to whoever made this request
