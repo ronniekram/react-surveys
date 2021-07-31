5a7c0cb5cd6a15c6f38dadd9e7e937f4d6bec578
Arynn Boniface <arynn.boniface@gmail.com>
2021-07-30 22:31:25 -0400

    with client build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+// const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -14,6 +15,8 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(express.json());
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -25,6 +28,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === "production") {
+  // make sure express serves up production assets like main.js, main.css
+  app.use(express.static('client/build'));
+  // express will serve up index.html if it doesnt recognize the routes
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -22,21 +22,17 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
-      proxy: true
+      proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // we already have a record with the given profile id
-            done(null, existingUser);
-          } else {
-            // we want to create a new user
-            new User({ googleId: profile.id })
-            .save() // creates a model instance - returns a promise
-            .then(user => done(null, user));
-          };
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
