525e72903d95f4b19984ef64875615a3df918153
Arynn Boniface <arynn.boniface@gmail.com>
2021-07-31 16:58:05 -0400

    trying to fix redux form peer deps

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,18 @@
-// SurveyForm shows a form for a user to add input
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-import validateEmails from '../../utils/validateEmails';
-import formFields from './formFields';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipeient List', name: 'emails' },
+];
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(formFields, ({ label, name }) => {
+    return _.map(FIELDS, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -25,37 +28,17 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
           {this.renderFields()}
-          <Link to="/surveys" className="red btn-flat white-text">
-            Cancel
-          </Link>
-          <button type="submit" className="teal btn-flat right white-text">
-            Next
-            <i className="material-icons right">done</i>
-          </button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
   }
 }
 
-function validate(values) {
-  const errors = {};
-
-  errors.recipients = validateEmails(values.recipients || '');
-
-  _.each(formFields, ({ name }) => {
-    if (!values[name]) {
-      errors[name] = 'You must provide a value';
-    }
-  });
-
-  return errors;
-}
-
 export default reduxForm({
-  validate,
   form: 'surveyForm',
-  destroyOnUnmount: false
 })(SurveyForm);
